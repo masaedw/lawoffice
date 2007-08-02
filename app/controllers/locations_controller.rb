a24dcde7b69faf1ed6fb0099bcbd2207ffc7e615
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-08-02 15:37:57 +0000

    meteor
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@13735 33691c03-620b-0410-8a39-d73902a51864

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,6 +8,16 @@ class LocationsController < ApplicationController
     params['locations-table'].each_with_index do |id, position|
       Location.update(id, :position => position)
     end
+    
+    @list = Location.find(:all, :order => 'position')
+
+    js = render_to_string :update do |page|
+      page.replace_html 'locations-table', render(:partial => 'item', :collection => @list)
+      page.sortable 'locations-table', :tag => 'tr', :url => {:controller => 'locations', :action => 'sort'}
+    end
+    
+    Meteor.shoot('lawoffice', js)
+
     render :nothing => true
   end
 
