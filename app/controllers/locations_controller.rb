fcacf766398e18cdea796a54fc53b9ab8e986405
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-08-02 06:59:32 +0000

    position update
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@13734 33691c03-620b-0410-8a39-d73902a51864

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,14 @@
 class LocationsController < ApplicationController
 
   def list
-    @list = Location.find(:all)
+    @list = Location.find(:all, :order => 'position')
   end
 
   def sort
+    params['locations-table'].each_with_index do |id, position|
+      Location.update(id, :position => position)
+    end
+    render :nothing => true
   end
 
   def new
