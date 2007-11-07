845ab0397bba26dd96f85a1746e381844fcd1b38
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-11-07 17:33:31 +0000

    hoge
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@13842 33691c03-620b-0410-8a39-d73902a51864

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var ZINDEXTOP = 100;
+
+Element.Methods.popup = function(element) {
+    $(element).setStyle({"zIndex": ZINDEXTOP++});
+    return element;
+};
+
+Element.addMethods();
