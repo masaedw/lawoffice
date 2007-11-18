3c3397ce40653bc7fa51ac567c5eb377a1034081
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-11-18 02:35:22 +0000

    filter
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@13864 33691c03-620b-0410-8a39-d73902a51864

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,9 @@
 class PeopleController < ApplicationController
+  before_filter :find_all, :only => [:list, :edit]
+  before_filter :find_id, :only => [:update_message, :update_position]
 
   def list
-    @list = Person.find(:all, :include => :location)
+    @list
   end
 
   def new
@@ -11,17 +13,16 @@ class PeopleController < ApplicationController
   end
 
   def edit
-    @list = Person.find(:all, :include => :location)
+    @list
     # update_remote のときに Draggable#destroy を呼ぶべきか調べる。
     # 必要なら呼ぶようにする。
   end
 
   def update_message
-    person = Person.find(params[:id])
-    person.message = params[:message]
-    if person.save
+    @person.message = params[:message]
+    if @person.save
       js = render_to_string :update do |page|
-        page << "Person.update_text('#{person.element_id}', 'message', '#{h person.message}');"
+        page << "Person.update_text('#{@person.element_id}', 'message', '#{h @person.message}');"
       end
       Meteor.shoot('lawoffice-view', js)
       Meteor.shoot('lawoffice-edit', js)
@@ -31,14 +32,13 @@ class PeopleController < ApplicationController
   end
 
   def update_position
-    person = Person.find(params[:id])
-    person.posx = params[:left].to_i
-    person.posy = params[:top ].to_i
-    if person.save
+    @person.posx = params[:left].to_i
+    @person.posy = params[:top ].to_i
+    if @person.save
       js = render_to_string :update do |page|
         page << <<-EOJ
-          $("#{person.element_id}").style.top = "#{person.top}px";
-          $("#{person.element_id}").style.left = "#{person.left}px";
+          $("#{@person.element_id}").style.top = "#{@person.top}px";
+          $("#{@person.element_id}").style.left = "#{@person.left}px";
         EOJ
       end
       Meteor.shoot('lawoffice-view', js)
@@ -49,5 +49,15 @@ class PeopleController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def find_all
+    @list = Person.find(:all, :include => :location)
+  end
+
+  def find_id
+    @person = Person.find(params[:id])
   end
 end
