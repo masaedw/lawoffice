c2964a5666dd070ad853d031049dd26345e9385a
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-11-22 18:39:57 +0000

    location addition is noticed by ajax
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@14627 33691c03-620b-0410-8a39-d73902a51864

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -31,7 +31,18 @@ class LocationsController < ApplicationController
 
   def create
     max = Location.maximum('position')
-    Location.create(:name => "新規", :listup => false, :color => "#eceef0", :position => max+1)
+    location = Location.create(:name => "新規", :listup => false, :color => "#eceef0", :position => max+1)
+
+    js = render_to_string :update do |page|
+      page << "if (!edit_mode) {"
+      page.insert_html :bottom, 'locations-table', render(:partial => 'item', :object => location)
+      page << "} else {"
+      page.insert_html :bottom, 'locations', render(:partial => 'edit', :object => location)
+      page.sortable 'locations', :tag => 'div', :url => {:controller => 'locations', :action => 'sort'}
+      page << "}"
+    end
+    shoot_both js
+
     render :nothing => true
   end
 
