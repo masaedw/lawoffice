de6f066114b9237c487dfcb4854c8c13b1705f2b
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2007-12-12 16:08:50 +0000

    hoge???
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@14731 33691c03-620b-0410-8a39-d73902a51864

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -7,13 +7,17 @@ class TemplatesController < ApplicationController
 
     @templates = Template.find(:all, :order => 'name')
 
-    # FIXME: IE でエラーになる
+    # FIXME: IE でselectが表示されない
+    # 1, IE で edit mode にアクセスし、「テンプレートの編集」ボタンを押さずに(ウインドウを開かずに)
+    # 他のクライアントでメモの作成を行い、更新処理を行わせると、テンプレートウインドウを表示したときに
+    # select が表示されない。
+    # メモの削除の場合は大丈夫。
+
     js = render_to_string :update do |page|
       page << "new MemoTemplate(#{template.element_id.to_json}, #{template.name.to_json}, #{template.color.to_json}, #{template.content.to_json});"
       page << "if (!edit_mode) {"
       page << "} else {"
       page << "var selected__ = $F('template_select');"
-      page.replace_html "template_select", ""
       page.replace_html "template_select", '<option value="0" id="template_select_none">新規テンプレート</option>'+options_from_collection_for_select(@templates, "id", "name")
       page << "var x = j$('#template_select option[@value=\#{id}]'.interpolate({id:selected__}));"
       page << "x[0].selected = true;"
@@ -23,7 +27,6 @@ class TemplatesController < ApplicationController
 
     render :update do |page|
       page << "new MemoTemplate(#{template.element_id.to_json}, #{template.name.to_json}, #{template.color.to_json}, #{template.content.to_json});"
-      page.replace_html "template_select", ""
       page.replace_html "template_select", '<option value="0" id="template_select_none">新規テンプレート</option>'+options_from_collection_for_select(@templates, "id", "name")
       page << "TemplateWindow.edit_mode(#{template.element_id.to_json});"
     end
