a375e690ffe7cbc89c7bb0375ea852c0803146d1
masa <masa@33691c03-620b-0410-8a39-d73902a51864>
2008-01-22 20:24:04 +0000

    ノートパッドがリサイズしないように
    git-svn-id: file:///Users/masa/.svk/local/local/lawoffice3/trunk@15606 33691c03-620b-0410-8a39-d73902a51864

--- public/javascripts/notepad.js
+++ public/javascripts/notepad.js
@@ -5,20 +5,8 @@
 var Notepad = new Object;
 
 Object.extend(Notepad, {
-  resize_callback: function() {
-    var width = $("notepad_area").getWidth();
-    $("notepad").setStyle({"width": width+"px"});
-  },
-
-  resize: function() {
-    Notepad.resizing.resize($("notepad_area"));
-    Notepad.resize_callback();
-  },
-
   init: function() {
-    Notepad.resizing = new ResizingTextArea('notepad_area', Notepad.resize_callback);
     new Draggable('notepad', {scroll: document.body});
-    Notepad.resize();
     Notepad.observer = new Form.Element.Observer('notepad_area', 2, function(element, value) {
       Notepad.update(value);
     }.bind(this));
